b0314e4736852e661fb7a91cac953612fcd0758f
Prakash Kumar Mandal <pramanda1@WKMZTE6D1DD8.global.publicisgroupe.net>
2022-06-10 14:32:33 +0530

    route setup done

---
+++ src/app/modules/public/Products.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products: React.FC<any> = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
---
+++ src/app/modules/public/Registration.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Registration: React.FC<any> = () => {
+  return (
+    <div>Registration</div>
+  )
+}
+
+export default Registration
