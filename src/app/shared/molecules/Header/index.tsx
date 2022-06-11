457276d0a38418f846a5b719ecb0b7999cd2e627
Prakash Kumar Mandal <pramanda1@WKMZTE6D1DD8.global.publicisgroupe.net>
2022-06-11 21:07:21 +0530

    header UI done

--- src/app/shared/molecules/Header/index.tsx
+++ src/app/shared/molecules/Header/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar, Nav, Offcanvas, Button } from "react-bootstrap";
-import { Logo } from "../atoms";
+import { Logo } from "../../atoms";
 import styled from "styled-components";
 import { LinkContainer } from "react-router-bootstrap";
 import { ROUTES } from "app/route/app-route-labels";
 import { BsFillCartFill } from "react-icons/bs";
 import breakpoints from "app/theme/breakpoints";
+import "./style.scss";
+import colors from "app/theme/colors";
 
 const Header: React.FC<any> = ({ cartCount }) => {
   return (
@@ -28,15 +30,21 @@ const Header: React.FC<any> = ({ cartCount }) => {
         <Offcanvas.Body>
           <Nav className="justify-content-end flex-grow-1 pe-3">
             <LinkContainer to={ROUTES.SIGN_IN}>
-              <NavLink href="#action1">Sign In</NavLink>
+              <NavLink href="#action1">Signin</NavLink>
             </LinkContainer>
             <LinkContainer to={ROUTES.REGISTRATION}>
               <NavLink href="#action2">Registration</NavLink>
             </LinkContainer>
+            <LinkContainer className="mobile_link" to={ROUTES.HOME}>
+              <NavLink>Home</NavLink>
+            </LinkContainer>
+            <LinkContainer className="mobile_link" to={ROUTES.PRODUCTS}>
+              <NavLink>Products</NavLink>
+            </LinkContainer>
           </Nav>
           <Button variant="light">
-            <BsFillCartFill />
-            {cartCount} Items
+            <BsFillCartFill color={colors.primary} />
+            <span>{cartCount} Items</span>
           </Button>
         </Offcanvas.Body>
       </Navbar.Offcanvas>
@@ -48,12 +56,18 @@ export default Header;
 
 const NavbarWhite = styled(Navbar)`
   background-color: #fff !important;
-  box-shadow: 0px 3px 17px 3px rgba(0,0,0,0.05);
+  box-shadow: 0px 3px 17px 3px rgba(0, 0, 0, 0.05);
+  @media screen and (min-width: ${breakpoints.lg}) {
+    padding-bottom: 0px;
+  }
 `;
 
 const NavLinkContainer = styled(Nav)`
   @media screen and (min-width: ${breakpoints.lg}) {
     margin-top: 20px;
+  }
+  @media screen and (max-width: ${breakpoints.lg}) {
+    display: none;
   }
 `;
 
