59e5a0455126eed3b8a69a81309ae5012a621006
Prakash Kumar Mandal <pramanda1@WKMZTE6D1DD8.global.publicisgroupe.net>
2022-06-12 16:48:01 +0530

    code cleaning done

--- src/app/shared/atoms/index.ts
+++ src/app/shared/atoms/index.ts
@@ -1,15 +1,11 @@
-import Button from "./Button";
 import Logo from "./Logo";
-import InputBox from "./InputBox";
 import LowestPriceGuaranteeCard from "./LowestPriceGuaranteeCard";
 import NoDataFound from "./NoDataFound";
 import IncrementDecrementBtn from "./IncrementDecrementBtn";
 
 export {
-  Button,
   NoDataFound,
   LowestPriceGuaranteeCard,
-  InputBox,
   Logo,
   IncrementDecrementBtn,
 };
